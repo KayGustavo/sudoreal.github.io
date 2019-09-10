11229daf9bb48bb3c81c10c36398c473b7e6a995
SudoReal <46944979+SudoReal@users.noreply.github.com>
2019-09-10 07:10:39 -0300

    Update appIndex.js

--- appIndex.js
+++ appIndex.js
@@ -1,3 +1,8 @@
+var root = null;
+var useHash = true; // Defaults to: false
+var hash = '#!'; // Defaults to: '#'
+var router = new Navigo(root, useHash, hash);
+
 router
   .on({
     'products/:id': function () {
